d6d57aa3ec294e560e94faff4fc8b6bdb84fab0e
kbzcraft <ytkbzcraft@gmail.com>
2023-08-17 18:18:23 +0545

    initial setup commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <h1>My portfolio website kbbohara</h1>
+    <h1 className='text-red-600'>My portfolio website kbbohara</h1>
   )
 }
 
